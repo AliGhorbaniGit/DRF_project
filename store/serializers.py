e31aa86c3b734b33ef798ad5c6a4ede2babd3f2c
ali <aghorbani778@gmail.com>
2024-06-04 20:09:04 +0330

    all serilizers created

--- store/serializers.py
+++ store/serializers.py
@@ -7,3 +7,42 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['title','unit_price','description','slug','category','discounts','inventory','datetime_created','datetime_modified',]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['title', 'description', 'top_product' ]
+
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['user' ,'product', 'body', 'datetime_created',  'status'   ]
+
+
+
+class CartSerilizer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ['id','created_at']
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['id','cart','product','quantity']
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['user', 'phone_number', 'birth_date'] 
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['customer', 'status', 'datetime_created'] 
+
