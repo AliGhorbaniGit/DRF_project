900f5fc1f6b64d1319f005f085450f55e438807c
ali <aghorbani778@gmail.com>
2024-06-04 19:29:22 +0330

    custom user added to core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+class CustomUser(AbstractUser):
+    """ this model created to make the user model as editable"""
+    email = models.EmailField(unique=True, verbose_name=_('email'))
