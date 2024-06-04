b18ccae1024f6f934a8ef8172d18334746af138e
ali <aghorbani778@gmail.com>
2024-06-04 20:21:06 +0330

    destroy method ovveride in productviewset

--- store/views.py
+++ store/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
+from rest_framework.response import Response
+from rest_framework import status
+
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Product, Category, Comment, Cart, CartItem, Customer, Order
-from .serializers import ProductSerializer 
 
+from .models import Product, Category, Comment, Cart, CartItem, Customer, Order
+from .serializers import ProductSerializer , CategorySerializer, CommentSerializer, CartSerilizer, CartItemSerializer,CustomerSerializer, OrderSerializer
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
-    filterset_fields = ['category','title','slug',]
-    search_fields = ['title',]
 
-    permission_classes = [IsAdminOrReadOnly]
+    def destroy(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        count = product.order_items.count()
+        if count > 0 :
+            return Response({'error': f'there is {count} order item related to this product please delete theme first'})
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
