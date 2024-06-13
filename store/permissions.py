4761ee20de5ce4045dbd4eff587459541e41117b
ali <aghorbani778@gmail.com>
2024-06-13 20:01:10 +0330

    add some new permissions to permissions.py

--- store/permissions.py
+++ store/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 from rest_framework.permissions import SAFE_METHODS
+import copy
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
@@ -9,7 +10,12 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         return bool(request.user and request.user.is_staff)
 
 
+class SendPrivateEmailTOCustomerPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.has_perm('store.send_private_email'))
 
 
-
-
+class CustomDjangoModelPermissions(permissions.BasePermission):
+    def __init__(self):
+        self.perms_map = copy.deepcopy(self.perms_map)
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
